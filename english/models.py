c434eb2145e2daaa2e9f00421d6cad02cf76a325
A. Eugen <Alekseew.zheka0207@gmail.com>
2024-08-17 20:04:28 +0300

    __init__ commit

---
+++ english/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class PPost(models.Model):
+    name = models.CharField(verbose_name='name_post',  max_length=50, default=None)
+    comment = models.CharField(max_length=500)
+    sentence = models.TextField(default=None)
+    publish_date = models.DateTimeField(default=timezone.now())
+    def __str__(self: str) -> str:
+        return self.name
